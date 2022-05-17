d1e129dbe59ea2361d85bf324dc7c7656840a37b
Imam Setiawan <s.imam29@gmail.com>
2022-05-18 06:19:09 +0700

    unit test user repository

---
+++ tests/Repository/UserRepositoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ProgrammerZamanNow\MVC\Repository;
+
+use PHPUnit\Framework\TestCase;
+use ProgrammerZamanNow\MVC\Config\Database;
+use ProgrammerZamanNow\MVC\Repository\UserRepository;
+use ProgrammerZamanNow\MVC\Domain\User;
+
+class UserRepositoryTest extends TestCase
+{
+    private UserRepository $userRepository;
+
+    public function setUp(): void
+    {
+        $this->userRepository = new UserRepository(Database::getConnection());
+        $this->userRepository->deleteAll();
+    }
+
+    public function testSaveSuccess()
+    {
+        $user = new User();
+        $user->id = 1;
+        $user->name = "Imam";
+        $user->password = "12345";
+
+        $this->userRepository->save($user);
+
+        $result = $this->userRepository->findById($user->id);
+
+        $this->assertEquals($user->id, $result->id);
+        $this->assertEquals($user->name, $result->name);
+        $this->assertEquals($user->password, $result->password);
+    }
+
+    public function testFindByIdNotFound()
+    {
+        $user = $this->userRepository->findById(100);
+        $this->assertNull($user);
+    }
+}
