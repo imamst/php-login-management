41b2624e9386a0a9af40f35078478f97cc35e28f
Imam Setiawan <s.imam29@gmail.com>
2022-05-21 08:47:11 +0700

    unit test user profile update repository

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -10,9 +10,13 @@ use ProgrammerZamanNow\MVC\Domain\User;
 class UserRepositoryTest extends TestCase
 {
     private UserRepository $userRepository;
+    private SessionRepository $sessionRepository;
 
     public function setUp(): void
     {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+        $this->sessionRepository->deleteAll();
+
         $this->userRepository = new UserRepository(Database::getConnection());
         $this->userRepository->deleteAll();
     }
@@ -22,7 +26,7 @@ class UserRepositoryTest extends TestCase
         $user = new User();
         $user->id = "imam";
         $user->name = "Imam";
-        $user->password = "12345";
+        $user->password = password_hash("12345", PASSWORD_BCRYPT);
 
         $this->userRepository->save($user);
 
@@ -37,5 +41,23 @@ class UserRepositoryTest extends TestCase
     {
         $user = $this->userRepository->findById(100);
         $this->assertNull($user);
+    }
+
+    public function testUpdate()
+    {
+        $user = new User();
+        $user->id = "imam";
+        $user->name = "Imam";
+        $user->password = password_hash("12345", PASSWORD_BCRYPT);
+        $this->userRepository->save($user);
+
+        $user->name = "Imam Setiawan";
+        $this->userRepository->update($user);
+
+        $result = $this->userRepository->findById($user->id);
+
+        $this->assertEquals($user->id, $result->id);
+        $this->assertEquals($user->name, $result->name);
+        $this->assertEquals($user->password, $result->password);
     }
 }
